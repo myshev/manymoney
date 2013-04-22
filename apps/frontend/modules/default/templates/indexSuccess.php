86965cbd003f80e9fcf6ce5470e8e590c64d0037
Myshev <sergey.myshev@softline.ru>
2013-04-22 21:47:58 +0400

    началное добавление верстки на сайт

---
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -0,0 +1,163 @@
+	<ul>
+
+		<li class="post" id="post-27">
+			<div class="date">24<span>Янв</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/privetsvuem-vas/" rel="bookmark" title="Постоянная ссылка: Приветствуем Вас!">Приветствуем Вас!</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-6/" title="Просмотреть все записи в рубрике «Рубрика 6»" rel="category tag">Рубрика 6</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/privetsvuem-vas/#comments" title="Комментарий к записи Приветствуем Вас!">Отзывов: 2</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2011/01/themes.jpg"><img class="alignleft size-medium wp-image-46" src="/uploads/tmp/themes-300x214.jpg" alt="" width="270" height="193"></a>На <a href="http://demo.wp-templates.ru/">данном сайте</a> вы можете протестировать, как работает понравившийся вам <em>шаблон</em>.<br>
+					Загрузить<span style="text-decoration: underline;"> </span>понравившийся шаблон можно вернувшись на <a href="http://wp-templates.ru/"><strong>основной сайт</strong></a>.</p>
+				<p>Чтобы облегчить поиск нужной вам темы для <strong>WordPress</strong> используйте навигацию сайта – облако тегов, категории и форму поиска.</p>
+				<p><strong>Посмотрим как выглядят заголовки:</strong></p>
+				<h1>Заголовок H1.</h1>
+				<h2>Заголовок H2.</h2>
+				<h3>Заголовок H3</h3>
+				<p>Пример того, как будут выглядеть списки.</p>
+				<ul>
+					<li>Небо</li>
+					<li>Земля</li>
+					<li>Вода</li>
+					<li>Огонь</li>
+				</ul>
+				<ol>
+					<li>Мир</li>
+					<li>Любовь</li>
+					<li>Радость</li>
+				</ol>
+				<p> <a href="http://demo.wp-templates.ru/privetsvuem-vas/#more-27" class="more-link">Continue&nbsp;reading&nbsp;»</a></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/wordpress/" rel="tag">WordPress</a>, <a href="http://demo.wp-templates.ru/tag/shablony/" rel="tag">Шаблоны</a></p>                	</li>
+
+		<li class="post" id="post-18">
+			<div class="date">24<span>Дек</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/ustanovka-shablonov-wordpress/" rel="bookmark" title="Постоянная ссылка: Установка шаблонов WordPress">Установка шаблонов WordPress</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-4/" title="Просмотреть все записи в рубрике «Рубрика 4»" rel="category tag">Рубрика 4</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/ustanovka-shablonov-wordpress/#respond" title="Комментарий к записи Установка шаблонов WordPress">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><strong><a href="http://demo.wp-templates.ru/wp-content/uploads/2010/12/3.jpg"><img class="alignleft size-medium wp-image-122" src="/uploads/tmp/3-300x225.jpg" alt="" width="263" height="197"></a>Установка тем не требует особых знаний и много времени:</strong></p>
+				<p>1. Скачайте шаблон на свой компьютер</p>
+				<p>2. Распакуйте архив с шаблоном</p>
+				<p>3. С помощью файлового менеджера (например Total Commander), загрузите папку с темой на ваш сайт в директорию wp-content/themes.</p>
+				<p>4. Зайдите в Панель управления сайта и в меню выберите опцию ВНЕШНИЙ ВИД — ТЕМЫ. Новый шаблон должен появиться в списке</p>
+				<p>5. Выберите новую тему и активируйте её.</p>
+				<p>6. Перейдите на Внешний вид — Виджеты и поставьте те виджеты, которые вам нужны</p>
+				<p>Все, установка темы закончена! Приятного просмотра.</p>
+				<p>Существует так же и другой способ. Зайдите в Внешний вид — Темы, в верхней части страницы перейдите в Установить темы, далее под появившемся вверху меню нужно следовать в Загрузка и там уже загрузить нужную тему в архиве zip.</p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/wordpress/" rel="tag">WordPress</a>, <a href="http://demo.wp-templates.ru/tag/ustanovka/" rel="tag">Установка</a></p>                	</li>
+
+		<li class="post" id="post-11">
+			<div class="date">24<span>Ноя</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/kak-vybrat-shablon-wordpress/" rel="bookmark" title="Постоянная ссылка: Как выбрать шаблон WordPress?">Как выбрать шаблон WordPress?</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-2/" title="Просмотреть все записи в рубрике «Рубрика 2»" rel="category tag">Рубрика 2</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/kak-vybrat-shablon-wordpress/#respond" title="Комментарий к записи Как выбрать шаблон WordPress?">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2010/11/5-beautiful-wordpress-themes.jpg"><img class="alignleft size-medium wp-image-67" src="/uploads/tmp/5-beautiful-wordpress-themes-300x138.jpg" alt="" width="300" height="138"></a>Первое место, куда в поисках темы для вордпрес идет каждый Blogger&nbsp;—  это архив шаблонов на официальном сайте WordPress. Основные преимущества  этого архива состоят в огромной базе доступных темам, а также  возможности сортировать темы по различным необходимыми вам критериям.</p>
+				<p>Впрочем, поскольку речь идет об официальной коллекции тем, то можно  обнаружить, что каждую из них загружали как минимум по несколько тысяч  раз и что самое главное они не на русском языке. А следовательно  подобных вашему блогу будет также немало. Для людей, которые не ищут  легких путей, существуют альтернативные «сайты с темами для WordPress». В  общем качайте шаблоны с нашего сайта и радуйтесь жизни!</p>
+				<p>Следует помнить, что зная наиболее общие принципы работы с HTML вы  сможете изменить каждую тему почти до неузнаваемости. Именно так, как  сделал я, когда создавал дизайн этого сайта. Для этого заходим в  редактор тем в админке WordPress (пункты меню «Вид» -&gt; «Редактор  тем») и изменяем необходимые места в коде. Прежде, чем это делать&nbsp;—  откройте возможность внешнего редактирования соответствующих файлов  Вордпрес. Но будте осторожны! Вы можете что называется «сломать свой  собственный сайт». <a href="http://demo.wp-templates.ru/kak-vybrat-shablon-wordpress/#more-11" class="more-link">Continue&nbsp;reading&nbsp;»</a></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/vybor/" rel="tag">выбор</a>, <a href="http://demo.wp-templates.ru/tag/shablony/" rel="tag">Шаблоны</a></p>                	</li>
+
+		<li class="post" id="post-38">
+			<div class="date">24<span>Окт</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/plaginy-dlya-wordpress/" rel="bookmark" title="Постоянная ссылка: Плагины для WordPress.">Плагины для WordPress.</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-5/podrubrika-1/" title="Просмотреть все записи в рубрике «Подрубрика 1»" rel="category tag">Подрубрика 1</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/plaginy-dlya-wordpress/#respond" title="Комментарий к записи Плагины для WordPress.">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2010/10/wordpress-plugin.jpg"><img class="alignleft size-medium wp-image-70" src="/uploads/tmp/wordpress-plugin-300x199.jpg" alt="" width="270" height="179"></a>1. <strong>RusToLat</strong>. Если вы планируете писать на русском  языке, то это первый плагин, который я советую установить сразу после  установки блога. Для украинских блоггеров существует аналог UkrToLat.  Плагин формирует адреса ваших постов, создавая транслитом их  кириллическое название. Например, если вы создадите запись под названием  «Тест» то без использования плагина его адрес будет выглядеть примерно  так: http://site.ru/2010/04/18/%D2%E5%F1%F2?, но с использованием  плагина она приобретет более читабельной вид:  http://site.ru/2010/04/18/test.</p>
+				<p>2. <strong>WordPress Database Backup</strong>. Плагин дает  возможность сделать бэкап вашей базы данных. Иными словами, если с вашим  блогом в будущем случится какая-нмбудь неприятность (никто не  застрахован) то вы сможете легко и просто восстановить то, что было  написано и обсуждено. Наконец, осторожность никому еще не помешала.</p>
+				<p>3. <strong>All in One SEO Pack</strong>. Через некоторое время после  установки блога вам придется задуматься о его продвижении. Упомянутый  плагин позволит максимально упростить эту задачу. Особенно, если вы  никогда не интересовались поисковой оптимизацией и толком не знаете, как  она работает. Плагин сделает все вместо вас.</p>
+				<p>4. <strong>Google XML Sitemaps</strong>. Цель этого плагина, как и  предыдущего&nbsp;— способствовать развитию и продвижению вашего блога. Как  известно, для того, чтобы ваш блог был включен в выдачу мощных  поисковиков&nbsp;— последним нужно проиндексировать его. Упомянутый плагин  облегчает эту задачу для роботов, создавая специальную «карту сайта».</p>
+				<p> <a href="http://demo.wp-templates.ru/plaginy-dlya-wordpress/#more-38" class="more-link">Continue&nbsp;reading&nbsp;»</a></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/wordpress/" rel="tag">WordPress</a>, <a href="http://demo.wp-templates.ru/tag/plaginy/" rel="tag">Плагины</a></p>                	</li>
+
+		<li class="post" id="post-75">
+			<div class="date">24<span>Окт</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/postoyannye-ssylki-v-wordpress/" rel="bookmark" title="Постоянная ссылка: Постоянные ссылки в WordPress">Постоянные ссылки в WordPress</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-1/" title="Просмотреть все записи в рубрике «Рубрика 1»" rel="category tag">Рубрика 1</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/postoyannye-ssylki-v-wordpress/#respond" title="Комментарий к записи Постоянные ссылки в WordPress">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2011/06/link-url.jpg"><img class="alignleft size-medium wp-image-76" src="/uploads/tmp/link-url-300x225.jpg" alt="" width="270" height="203"></a>Сразу после установки WordPress любая ссылка на посты имеет следующий  вид www.site.ru/?p=1. Согласитесь, не слишком привлекательно и удобно.  Хватит даже надеяться, что кто-то из ваших посетителей запомнит это  ссылки. А говорят, что поисковым машинам такой подход к формированию  ссылок также не очень нравится.</p>
+				<p>К счастью, выход есть! Для этого нам нужно настроить постоянные  ссылки. А хорошая новость заключается в том, что WordPress позволяет  настроить структуру ссылок любым удобным для нас образом.</p>
+				<p>Затягивать с установкой постоянных ссылок не нужно, поскольку, если  ваш блог окажется по-настоящему интересным&nbsp;— на его материалы сразу  появятся ссылки с других блогов. Понятное дело, если после этого вы  измените структуру ссылок то эти ссылки поведут «в пустоту». Поэтому я  советую настраивать структуру ссылок в первую очередь&nbsp;— возможно, даже  еще до того, как выберете для себя тему оформления и установите все  необходимые плагины. И в любом случае&nbsp;— до того, как разместите на блоге  первую не тестовую статью.</p>
+				<p><strong> <a href="http://demo.wp-templates.ru/postoyannye-ssylki-v-wordpress/#more-75" class="more-link">Continue&nbsp;reading&nbsp;»</a></strong></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/nastrojka/" rel="tag">Настройка</a>, <a href="http://demo.wp-templates.ru/tag/ssylki/" rel="tag">ссылки</a></p>                	</li>
+
+		<li class="post" id="post-8">
+			<div class="date">24<span>Окт</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/kopirajter-%e2%80%93-eto-zvuchit-gordo/" rel="bookmark" title="Постоянная ссылка: Копирайтер – это звучит гордо.">Копирайтер – это звучит гордо.</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-1/" title="Просмотреть все записи в рубрике «Рубрика 1»" rel="category tag">Рубрика 1</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/kopirajter-%e2%80%93-eto-zvuchit-gordo/#respond" title="Комментарий к записи Копирайтер – это звучит гордо.">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2011/01/1272361044_7.jpg"><img class="alignleft size-medium wp-image-9" src="/uploads/tmp/1272361044_7-300x204.jpg" alt="" width="240" height="163"></a>Ничто так не влияет на нашу жизнь как слово, облаченное в форму текста. Желающие оспорить этот факт, могут убедиться в вышесказанном, открыв, например конституцию или другие правовые и нормативные документы. Все что в них написано было, когда то словами и мыслями, которые в дальнейшем были перенесены на бумагу. Как бы ни была интересна мысль или высказывание, что бы доходчиво донести ее до людей, требуется все это красиво и доходчиво изложить на бумаге, которая впоследствии приобретя форму книги, газеты или другого издания попадает к нам в руки.</p>
+				<p>С приходом Интернета в нашу жизнь возможность получения информации в виде печатного текста становится поистине безграничной. Но этот текст, кто-то еще должен написать. Тут то и выходит на сцену такой человек как копирайтер, человек который занимается тем, что пишет авторские тексты. До появления Интернета этим в основном занимались профессиональные журналисты и писатели. И Интернет вначале тоже пользовался услугами копирайтеров в ограниченном количестве, потому что на тот момент существовало огромное количество печатной продукции, информацию, с которой и переносили в интернет. Но так долго продолжатся, не могло.</p>
+				<p> <a href="http://demo.wp-templates.ru/kopirajter-%e2%80%93-eto-zvuchit-gordo/#more-8" class="more-link">Continue&nbsp;reading&nbsp;»</a></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/kopirajter/" rel="tag">Копирайтер</a></p>                	</li>
+
+		<li class="post" id="post-87">
+			<div class="date">24<span>Сен</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/pravila-vybora-xostinga-dlya-sajta/" rel="bookmark" title="Постоянная ссылка: Правила выбора хостинга для сайта">Правила выбора хостинга для сайта</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-1/" title="Просмотреть все записи в рубрике «Рубрика 1»" rel="category tag">Рубрика 1</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/pravila-vybora-xostinga-dlya-sajta/#respond" title="Комментарий к записи Правила выбора хостинга для сайта">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2011/06/cloud-computing.jpg"><img class="alignleft size-medium wp-image-88" src="/uploads/tmp/cloud-computing-300x168.jpg" alt="" width="270" height="151"></a>Много хостеров предлагают зарегистрировать доменное имя со скидками.  Этого делать не нужно. Регистрируйте домены отдельно у стороннего  регистратора.</p>
+				<p>Если воспользоваться услугой регистрации доменного имени у хостера,  то можно получить множество проблем, связанных с возможным переездом  вашего сайта на другой хостинг.</p>
+				<p>Если вы не обладаете достаточным знанием английского языка, то никогда не пользуйтесь услугами зарубежного хостера.</p>
+				<p>Связываться со службой поддержки рано или поздно придется, а без  должного знания языка вы не сможете объяснить специалисту суть проблемы и  не получите ответа на вопрос, что вам нужно сделать для ее устранения.</p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/sajt/" rel="tag">сайт</a>, <a href="http://demo.wp-templates.ru/tag/xosting/" rel="tag">хостинг</a></p>                	</li>
+
+		<li class="post" id="post-97">
+			<div class="date">24<span>Сен</span></div>
+			<div class="title">
+				<h2><a href="http://demo.wp-templates.ru/sozdaem-rezervnuyu-kopiyu-sajta-wordpress/" rel="bookmark" title="Постоянная ссылка: Создаем резервную копию сайта WordPress">Создаем резервную копию сайта WordPress</a></h2>
+			</div>
+			<div class="postdata">
+				<span class="category"><a href="http://demo.wp-templates.ru/category/rubrika-1/" title="Просмотреть все записи в рубрике «Рубрика 1»" rel="category tag">Рубрика 1</a></span>
+				<span class="comments"><a href="http://demo.wp-templates.ru/sozdaem-rezervnuyu-kopiyu-sajta-wordpress/#respond" title="Комментарий к записи Создаем резервную копию сайта WordPress">Ваш отзыв</a></span>
+			</div>
+			<div class="entry">
+				<p><a href="http://demo.wp-templates.ru/wp-content/uploads/2011/06/168.jpg"><img class="alignleft size-medium wp-image-98" src="/uploads/tmp/168-300x160.jpg" alt="" width="270" height="144"></a>Всем известен плагин <strong>WP-DB-Backup</strong>, отвечающий за создание резеврной копии базы данных сайта на WordPress.</p>
+				<p>Но есть не менее полезный плагин для резервирования основных директорий, которые составляют индивидуальность сайта: папки плагинов, темы, папки с картинками. Плагин называется WordPress Backup.</p>
+				<p>Просто загружаем оба плагина в папку плагинов, или добавляем через меню Плагины -&gt; Добавить новый в админке WordPress.Затем активируем.</p>
+				<p>Плагин резервирования базы данных находится в меню Инструменты -&gt; Архивация базы данных (или Резервное копирование).</p>
+				<p>Плагин резервирования файлов сайта находится в меню Настройки -&gt; WordPress Backup.</p>
+				<p>Для обоих плагинов можно настроить автоматическое резервирование 1 раз в день, или раз в неделю, например, и указать в настройках адрес для пересылки резервной копии на почту.</p>
+				<p> <a href="http://demo.wp-templates.ru/sozdaem-rezervnuyu-kopiyu-sajta-wordpress/#more-97" class="more-link">Continue&nbsp;reading&nbsp;»</a></p>
+			</div>
+			<p>Метки: <a href="http://demo.wp-templates.ru/tag/bezopasnost/" rel="tag">безопасность</a></p>                	</li>
+	</ul>
+
+	<p><a href="http://demo.wp-templates.ru/page/2/?wptheme=Cloudy">« Предыдущая страница</a>&nbsp;&nbsp;&nbsp;</p>
